1c65c339b36efeda4cf4ad17833bc2e0addfa609
eyakubsorkar <eyakubsorkar@gmail.com>
2019-10-17 19:03:51 +0600

    fetching data

--- task/views.py
+++ task/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .models import *
+import requests
+import pprint
 
 # Create your views here.
+
+
+class Index(View):
+    def get(self, request):
+        random_user = requests.get('https://randomuser.me/api/')
+        random_user = random_user.json()
+
+        gender = Gender()
+        gender.type = random_user.get('results')[0]['gender']
+
+        name = Name()
+        name.title = random_user.get('results')[0]['name']['title']
+        name.first = random_user.get('results')[0]['name']['first']
+        name.last = random_user.get('results')[0]['name']['last']
+
+        return HttpResponse('hi there')
+
+    def post(self, request):
+        pass
