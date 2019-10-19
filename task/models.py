5960539a31dc93e91f0c09f0eb34e4baeb0c4f48
eyakubsorkar <eyakubsorkar@gmail.com>
2019-10-19 18:56:32 +0600

    done getting data

--- task/models.py
+++ task/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Gender(models.Model):
-    type = models.CharField(max_length=15, null=True)
+    type = models.CharField(max_length=155, null=True)
 
     def __str__(self):
         return self.type
